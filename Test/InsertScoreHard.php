046c42fcf578758ad6506c19258e206cc69e1e47
mockiis <whampi.mocke@gmail.com>
2016-05-03 15:05:12 +0200

    q

--- Test/InsertScoreHard.php
+++ Test/InsertScoreHard.php
@@ -1,16 +1,21 @@
-<?php   
-	$db = new DB_CONNECT();  
-      $result = mysql_query("INSERT INTO Scores (name, score) VALUES('Darius', '2000')");  
-      // check if row inserted or not  
-      if ($result) {  
-         $response["success_msg"] = 1;  
-         $response["message"] = "Product successfully Insert.";  
-         echo json_encode($response);  
-      } else {  
-         // failed to insert row  
-         $response["success_msg "] = 0;  
-         $response["message"] = "Product not insert because Oops! An error occurred.";  
-         // echoing JSON response  
-         echo json_encode($response);  
-      }  
-?> 
+<?php
+$username = "whampi";
+$password = "Dalaplan2000";
+$hostname = "mysql.whampi.com"; 
+
+//connection to the database
+$dbhandle = mysql_connect($hostname, $username, $password) 
+ or die("Unable to connect to MySQL");
+echo "Connected to MySQL<br>";
+
+//select a database to work with
+$selected = mysql_select_db("dbWhampi1",$dbhandle) 
+  or die("Could not select dbWhampi1");
+
+//execute the SQL query and return records
+$result = mysql_query("INSERT INTO Scores (name, score) VALUES( Darius, 2000)");
+
+}
+//close the connection
+mysql_close($dbhandle);
+?>
